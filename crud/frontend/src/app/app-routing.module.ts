8c83a501b24d87b0df92f099f73a536241741b85
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-01-30 15:53:13 -0300

    Upgraded router

--- crud/frontend/src/app/app-routing.module.ts
+++ crud/frontend/src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductCrudComponent } from './product-crud/product-crud.component';
-import { ProductCreateComponent } from './product-crud/product-create/product-create.component';
-import { ProductUpdateComponent } from './product-crud/product-update/product-update.component';
-import { ProductDeleteComponent } from './product-crud/product-delete/product-delete.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "/home", pathMatch: "full"},
-  {path: "home", component: HomeComponent},
+  {path: "", component: HomeComponent},
 ];
 
 @NgModule({
